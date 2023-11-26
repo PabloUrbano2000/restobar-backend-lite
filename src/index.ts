d16b4dc783d7fb5c019a3711b51d5acf93e57290
Pablo El Maldy <pablojamiro2008@hotmail.com>
2023-11-26 15:48:48 -0500

    feat: agregando rutas de categorias, tipos de doc, generos, recepciones y auth de clientes

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,35 @@ import dotenv from "dotenv";
 import Server from "./models/Server";
 import { executeData, deleteData } from "./seed/functions";
 
-import { Firebase } from "./firebase";
-import { DOCUMENT_TYPE_COLLECTION } from "./models/DocumentType";
+// import { Firebase } from "./firebase";
+// import { DOCUMENT_TYPE_COLLECTION } from "./models/DocumentType";
 
 dotenv.config();
 
-// if (process.argv[2]) {
-//   if (process.argv[2] === "--insert-seed") {
-//     console.log("ejecutando flag:", process.argv[2]);
-//     executeData();
-//   } else if (process.argv[2] === "--delete-seed") {
-//     console.log("ejecutando flag:", process.argv[2]);
-//     deleteData();
-//   }
-// } else {
-//   const server = new Server();
-//   server.execute();
-// }
-
-async function perro() {
-  const firebase = new Firebase();
-  const updated = new Date("2023-11-26")
-  const result = await firebase.getDocumentsByFilter(
-    DOCUMENT_TYPE_COLLECTION,
-    [["updated_date", ">=", updated]],
-    // [["name", "asc"]],
-    [],
-    { limit: 10, offset: 0 }
-  );
-  console.log(result);
+if (process.argv[2]) {
+  if (process.argv[2] === "--insert-seed") {
+    console.log("ejecutando flag:", process.argv[2]);
+    executeData();
+  } else if (process.argv[2] === "--delete-seed") {
+    console.log("ejecutando flag:", process.argv[2]);
+    deleteData();
+  }
+} else {
+  const server = new Server();
+  server.execute();
 }
 
-perro();
+// async function perro() {
+//   const firebase = new Firebase();
+//   const updated = new Date("2023-11-26")
+//   const result = await firebase.getDocumentsByFilter(
+//     DOCUMENT_TYPE_COLLECTION,
+//     [["updated_date", ">=", updated]],
+//     // [["name", "asc"]],
+//     [],
+//     { limit: 10, offset: 0 }
+//   );
+//   console.log(result);
+// }
+
+// perro();
